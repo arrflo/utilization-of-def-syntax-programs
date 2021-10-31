b190a93c11a98002c1e0136faa256314f4a70b09
romalynfloranda <romalynfloranda0203@gmail.com>
2021-10-31 17:54:09 +0800

    apple.price.change

---
+++ program3.py
@@ -0,0 +1,30 @@
+
+def getMoney():
+    _money = float(input("Amount of money you have:"))
+    return _money
+
+def getPrice ():
+    _applePrice = float (input("Price of apple per piece:"))
+    return _applePrice
+
+def getAppleMax ():
+    _applemax = money / price
+    applerounded = int (_applemax)
+    return applerounded
+
+def getChange ():
+    ttlPayment = appmax * price
+    _change = money - ttlPayment
+    return _change
+
+def display (maxNumberApples,change ):
+    print("You can buy", f"{maxNumberApples}" , "and your change is", f"{change:.2f} pesos.")
+
+
+money = getMoney ()
+price = getPrice ()
+appmax = getAppleMax ()
+chAnge = getChange ()
+
+display (appmax, chAnge)
+
